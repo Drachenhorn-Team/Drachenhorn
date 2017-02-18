f5792d8d31365299b93483efebe22aec39aa1204
lightlikeD <jemand10.6@web.de>
2017-02-19 00:15:36 +0100

    renaming

---
+++ DSACharacterSheet/CharacterSheet/ProfessionInformation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DSACharacterSheet.CharacterSheet
+{
+    public class ProfessionInformation : INotifyPropertyChanged
+    {
+        [XmlIgnore]
+        private string _name;
+        [XmlAttribute("Name")]
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name == value)
+                    return;
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+
+        #region OnPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion OnPropertyChanged
+    }
+}
