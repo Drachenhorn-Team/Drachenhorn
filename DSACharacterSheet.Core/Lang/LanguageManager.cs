a55524bec1f49a3646b476436a7619b9f954955d
lightlike <dnietfeld@outlook.com>
2017-11-09 10:14:43 +0100

    closed #10

---
+++ DSACharacterSheet.Core/Lang/LanguageManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSACharacterSheet.Core.Lang
+{
+    public class LanguageManager
+    {
+        public string this[string key]
+        {
+            get { return GetLanguageText(key); }
+        }
+
+        public static string GetLanguageText(string identifier)
+        {
+            ResourceManager rm = new ResourceManager("DSACharacterSheet.Core.Lang.lang", typeof(LanguageManager).Assembly);
+            try
+            {
+                return rm.GetString(identifier, CultureInfo.CurrentUICulture);
+            }
+            catch (MissingManifestResourceException)
+            {
+                return identifier;
+            }
+        }
+    }
+}
---
+++ DSACharacterSheet.Desktop/Lang/TranslateExtension.cs
@@ -0,0 +1,18 @@
+﻿using DSACharacterSheet.Core.Lang;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DSACharacterSheet.Desktop.Lang
+{
+    public class TranslateExtension : Binding
+    {
+        public TranslateExtension(string name) : base("[" + name + "]")
+        {
+            this.Source = new LanguageManager();
+        }
+    }
+}
