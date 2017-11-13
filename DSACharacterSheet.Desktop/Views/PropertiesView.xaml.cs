75d926d73329d1b08046f4c133e9f673256fc6c6
lightlike <dnietfeld@outlook.com>
2017-11-13 13:33:48 +0100

    changed icons added about changed main ui

--- DSACharacterSheet.Desktop/Views/PropertiesView.xaml.cs
+++ DSACharacterSheet.Desktop/Views/PropertiesView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using DSACharacterSheet.Desktop.Settings;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace DSACharacterSheet.Desktop.Views
@@ -24,6 +26,13 @@ namespace DSACharacterSheet.Desktop.Views
         {
             InitializeComponent();
             this.DataContext = PropertiesManager.Properties;
+        }
+
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
         }
     }
 }
