db3894836393262b9fe8e8b5ded6961c9400aab9
lightlike <dnietfeld@outlook.com>
2017-11-16 11:21:07 +0100

    test

--- DSACharacterSheet.Desktop/App.xaml.cs
+++ DSACharacterSheet.Desktop/App.xaml.cs
@@ -21,6 +21,8 @@ namespace DSACharacterSheet.Desktop
 
         private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
         {
+            e.Handled = true;
+
             var window = new ExceptionMessageBox(e.Exception, "Im Programm ist ein Fehler aufgetreten.");
             window.Show();
         }
