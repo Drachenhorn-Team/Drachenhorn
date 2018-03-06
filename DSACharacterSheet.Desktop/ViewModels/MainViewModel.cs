6a55a57fd6b85bd2cbde5be3c37149646f4aa979
lightlike <dnietfeld@outlook.com>
2018-03-06 10:27:52 +0100

    added mvvm

---
+++ DSACharacterSheet.Desktop/ViewModels/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSACharacterSheet.Desktop.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        #region Properties
+
+        private ObservableCollection<CharacterSheetViewModel> _characterSheetViewModels = new ObservableCollection<CharacterSheetViewModel>();
+        public ObservableCollection<CharacterSheetViewModel> CharacterSheetViewModels
+        {
+            get { return _characterSheetViewModels; }
+            set
+            {
+                if (_characterSheetViewModels == value)
+                    return;
+                _characterSheetViewModels = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        #endregion Properties
+    }
+}
