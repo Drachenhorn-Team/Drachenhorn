e3bc8cf7e9ce54209538f5a16aa97adccb164086
lightlike <dnietfeld@outlook.com>
2018-05-23 11:51:36 +0200

    prepare for renaming

--- DSACharacterSheet.Desktop/Properties/AssemblyInfo.cs
+++ DSACharacterSheet.Desktop/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Windows;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("Drachenhorn.Desktop")]
+[assembly: AssemblyTitle("Drachenhorn")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
