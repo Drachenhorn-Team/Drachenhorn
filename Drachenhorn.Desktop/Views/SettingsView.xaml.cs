a15292c15e7539db17e4ac160892de48cec897bb
DanielN <dnietfeld@outlook.com>
2018-10-04 11:49:54 +0200

    clickonce testing

--- Drachenhorn.Desktop/Views/SettingsView.xaml.cs
+++ Drachenhorn.Desktop/Views/SettingsView.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Deployment.Application;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
