756c2b2f878838372d62a1d73b412aff285fefc6
lightlike <dnietfeld@outlook.com>
2019-06-05 09:10:40 +0200

    Template restructure

--- Drachenhorn.Xml/Sheet/Common/ProfessionInformation.cs
+++ Drachenhorn.Xml/Sheet/Common/ProfessionInformation.cs
@@ -13,17 +13,13 @@ namespace Drachenhorn.Xml.Sheet.Common
     /// <seealso cref="Drachenhorn.Xml.ChildChangedBase" />
     /// <seealso cref="Drachenhorn.Xml.Interfaces.IInfoObject" />
     [Serializable]
-    public class ProfessionInformation : ChildChangedBase, IInfoObject
+    public class ProfessionInformation : GenerationDataHolder, IInfoObject
     {
         #region Properties
 
-        [XmlIgnore] private ObservableCollection<BonusValue> _baseValues = new ObservableCollection<BonusValue>();
+        [XmlIgnore] private string _name;
 
         [XmlIgnore] private string _description;
-
-        [XmlIgnore] private double _gpCost;
-
-        [XmlIgnore] private string _name;
 
         /// <summary>
         ///     Gets or sets the name.
@@ -63,55 +59,20 @@ namespace Drachenhorn.Xml.Sheet.Common
             }
         }
 
-        /// <summary>
-        ///     Gets or sets the gp cost.
-        /// </summary>
-        /// <value>
-        ///     The gp cost.
-        /// </value>
-        [XmlAttribute("GPCost")]
-        public double GpCost
-        {
-            get => _gpCost;
-            set
-            {
-                if (_gpCost == value)
-                    return;
-                _gpCost = value;
-                OnPropertyChanged();
-            }
-        }
-
-        /// <summary>
-        ///     Gets or sets the base values.
-        /// </summary>
-        /// <value>
-        ///     The base values.
-        /// </value>
-        [XmlElement("BaseValue")]
-        public ObservableCollection<BonusValue> BaseValues
-        {
-            get => _baseValues;
-            set
-            {
-                if (_baseValues == value)
-                    return;
-                _baseValues = value;
-                OnPropertyChanged();
-            }
-        }
-
         #endregion
 
         /// <inheritdoc />
-        public Dictionary<string, string> GetInformation()
+        public new Dictionary<string, string> GetInformation()
         {
             var result = new Dictionary<string, string>();
 
             if (!string.IsNullOrEmpty(Name)) result.Add("%Info.Name", Name);
             if (!string.IsNullOrEmpty(Description)) result.Add("%Info.Description", Description);
-            if (Math.Abs(GpCost) > double.Epsilon)
-                result.Add("%Info.GPCost", GpCost.ToString(CultureInfo.CurrentCulture));
+
+            foreach (var info in base.GetInformation())
+            {
+                result.Add(info.Key, info.Value);
+            }
 
             return result;
         }
---
+++ Drachenhorn.Xml/Sheet/GenerationDataHolder.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Text;
+using System.Xml.Serialization;
+using Drachenhorn.Xml.Interfaces;
+using Drachenhorn.Xml.Sheet.Common;
+using Drachenhorn.Xml.Sheet.Skills;
+
+namespace Drachenhorn.Xml.Sheet
+{
+    public class GenerationDataHolder : ChildChangedBase, IInfoObject
+    {
+        #region Properties
+
+        [XmlIgnore] private int _generationCost;
+
+        [XmlIgnore] private ObservableCollection<BonusValue> _baseValues = new ObservableCollection<BonusValue>();
+
+        [XmlIgnore] private ObservableCollection<Skill> _skills = new ObservableCollection<Skill>();
+
+        /// <summary>
+        ///     Gets or sets the gp cost.
+        /// </summary>
+        /// <value>
+        ///     The gp cost.
+        /// </value>
+        [XmlAttribute("GenerationCost")]
+        public int GenerationCost
+        {
+            get => _generationCost;
+            set
+            {
+                if (_generationCost == value)
+                    return;
+                _generationCost = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        ///     Gets or sets the base values.
+        /// </summary>
+        /// <value>
+        ///     The base values.
+        /// </value>
+        [XmlElement("BaseValue")]
+        public ObservableCollection<BonusValue> BaseValues
+        {
+            get => _baseValues;
+            set
+            {
+                if (_baseValues == value)
+                    return;
+                _baseValues = value;
+                OnPropertyChanged();
+            }
+        }
+
+        [XmlElement("Skill")]
+        public ObservableCollection<Skill> Skills
+        {
+            get => _skills;
+            set
+            {
+                if (_skills == value)
+                    return;
+                _skills = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        /// <inheritdoc />
+        public Dictionary<string, string> GetInformation()
+        {
+            var result = new Dictionary<string, string>();
+            
+            if (GenerationCost != 0)
+                result.Add("%Info.GPCost", GenerationCost.ToString(CultureInfo.CurrentCulture));
+
+            foreach (var baseValue in BaseValues)
+                result.Add(baseValue.Name, baseValue.Value.ToString());
+
+            foreach (var skill in Skills)
+                result.Add(skill.Name, skill.Value.ToString());
+
+            return result;
+        }
+    }
+}
